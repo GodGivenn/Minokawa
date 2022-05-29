eee3f6f8af14020d75daa16292476a7567f38eaa
GodGivenn <balicabogdaniulian@gmail.com>
2022-05-29 20:27:12 +0300

    Quick commit

---
+++ Minokawa/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D body;
+
+    private void Awake()
+    {
+        body = GetComponent<RigidBody2D>();
+    }
+
+    private void Update()
+    {
+        body.velocity = new Vector2(Input.GetAxis("Horizontal"), body.velocity.y);
+    }
+}
